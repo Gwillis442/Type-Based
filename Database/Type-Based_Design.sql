93efad6ac0ab435101414b832262979f27bd884c
Gwillis442 <gwillis501@gmail.com>
2024-10-31 08:32:27 -0700

    created Database Design and Relation structure

---
+++ Database/Type-Based_Design.sql
@@ -0,0 +1,52 @@
+drop table typing_stats;
+drop table math_stats;
+drop table user_config;
+drop table user_account;
+drop table user_profile;
+
+CREATE TABLE user_profile(
+    user_id VARCHAR2(8) PRIMARY KEY,
+    first_name VARCHAR2(50),
+    last_name VARCHAR2(50),
+    email VARCHAR2(50)
+);
+
+CREATE TABLE user_account(
+    user_id VARCHAR2(8) PRIMARY KEY,
+    username VARCHAR2(50),
+    password VARCHAR2(50),
+    FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
+);
+
+CREATE TABLE user_config(
+    user_id VARCHAR2(8) PRIMARY KEY,
+    default_mode VARCHAR2(50),
+    sound int,
+    music int,
+    play_history int,
+    FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
+);
+
+CREATE TABLE math_stats(
+    user_id VARCHAR2(8) PRIMARY KEY,
+    accuracy float(1),
+    total_games_easy int, 
+    total_games_medium int,
+    total_games_hard int,
+    high_score_easy int,
+    high_score_medium int,
+    high_score_hard int,
+    FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
+);
+
+CREATE TABLE typing_stats(
+    user_id VARCHAR2(8) PRIMARY KEY,
+    wpm int,
+    total_games_easy int,
+    total_games_medium int,
+    total_games_hard int,
+    high_score_easy int,
+    high_score_medium int,
+    high_score_hard int,
+    FOREIGN KEY (user_id) REFERENCES user_profile(user_id)
+);
